ea0bc5a84a9c21b795d02bad80b7cc22132fae42
Jose Gonzalez <josegzzv@msn.com>
2024-01-02 18:14:02 -0600

    fixed route index.html

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/" element={<Home />} />
                 <Route path="" element={<Home />} />
+                <Route path="/index.html" element={<Home />} />
               </Routes>
             </main>
             <Footer />
